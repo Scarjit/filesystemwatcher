5b024ad5a6ae09350ca5400f865df78bc3213b00
Scarjit <scarjit@aol.com>
2020-03-31 13:30:46 +0200

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,45 @@
-use std::collections::HashMap;
 use config::ConfigError;
-use std::path::Path;
-use std::{thread, io};
+use notify::{Event, EventKind, RecommendedWatcher, RecursiveMode, Watcher};
+use rodio::{Decoder, Device, Source};
+use std::collections::HashMap;
 use std::fs::File;
-use std::io::{BufReader, Write, Read};
-use rodio::{Source, Device, Decoder};
+use std::io::{BufReader, Read, Write};
+use std::path::Path;
 use std::sync::mpsc::channel;
 use std::time::Duration;
-use notify::{RecommendedWatcher, Watcher, RecursiveMode, Event, EventKind};
+use std::{io, thread};
 
 fn main() {
     let mut settings = config::Config::default();
     settings
-        .merge(config::File::with_name("settings")).unwrap()
-        .merge(config::Environment::with_prefix("APP")).unwrap();
+        .merge(config::File::with_name("settings"))
+        .unwrap()
+        .merge(config::Environment::with_prefix("APP"))
+        .unwrap();
 
-
-
-    if let Ok(highlightspath_str) = settings.get_str("path"){
+    if let Ok(highlightspath_str) = settings.get_str("path") {
         let record_path = Path::new(&highlightspath_str);
-        if !record_path.exists(){
+        if !record_path.exists() {
             println!("Invalid replay path !");
-        }else {
-            let mut watcher: RecommendedWatcher = Watcher::new_immediate(|res| {
-                match res {
-                    Ok(event) => play_sound(),
-                    Err(e) => println!("watch error: {:?}", e),
-                }
-            }).unwrap();
+        } else {
+            let mut watcher: RecommendedWatcher = Watcher::new_immediate(|res| match res {
+                Ok(event) => play_sound(),
+                Err(e) => println!("watch error: {:?}", e),
+            })
+            .unwrap();
 
-            watcher.watch(highlightspath_str, RecursiveMode::Recursive).unwrap();
+            watcher
+                .watch(highlightspath_str, RecursiveMode::Recursive)
+                .unwrap();
 
             loop {}
         }
-
-    }else{
+    } else {
         println!("NVIDIA Highlights path missing");
     }
 }
 
-pub fn play_sound(){
+pub fn play_sound() {
     let device = rodio::default_output_device().unwrap();
     let source = rodio::Decoder::new(BufReader::new(File::open(&"sound.wav").unwrap())).unwrap();
     rodio::play_raw(&device, source.convert_samples());
